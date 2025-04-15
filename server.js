8f2682b37320da3a02c823fd2b5ed8723c347318
qrxx <61294815+qrxx@users.noreply.github.com>
2025-04-15 23:20:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -100,5 +100,5 @@ app.get('/proxy', async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
